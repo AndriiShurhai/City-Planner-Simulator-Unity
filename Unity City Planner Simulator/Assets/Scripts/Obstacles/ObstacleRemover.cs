fbfd49aff6fb9efb42e6601e026dd0723d948280
Andrii Shurhai <shurgaiandrew335@gmail.com>
2025-01-31 11:11:43 +0200

    Updating Obstacle Remover, adding new obstacle, separating different kinds of obstacles into different tilemaps.

--- Unity City Planner Simulator/Assets/Scripts/Obstacles/ObstacleRemover.cs
+++ Unity City Planner Simulator/Assets/Scripts/Obstacles/ObstacleRemover.cs
@@ -9,13 +9,18 @@ using UnityEngine.UI;
 public class ObstacleRemover : MonoBehaviour
 {
     [SerializeField] private Tilemap obstacleTilemap;
+    [SerializeField] private Tilemap smallObstacleTilemap;
+    [SerializeField] private Tilemap middleObstacleTilemap;
+    [SerializeField] private Tilemap largeObstacleTilemap;
     [SerializeField] private RectTransform contextMenuPanel;
     [SerializeField] private Button removeButton;
     [SerializeField] private GameObject removeEffectPrefab;
-    [SerializeField] private int removalCost = 100;
+    [SerializeField] private int removalCost;
 
+    private Tilemap currentObstacleTilemap;
     private Vector3Int selectedCell;
     private Vector3Int lastHoveredCell;
+    private bool isHovering = false;
 
     private Camera mainCamera;
 
@@ -30,10 +35,10 @@ public class ObstacleRemover : MonoBehaviour
 
     private void OnRemoveClicked()
     {
-        obstacleTilemap.SetTile(selectedCell, null);
+        currentObstacleTilemap.SetTile(selectedCell, null);
         contextMenuPanel.gameObject.SetActive(false);
         AudioManager.Instance.PlayRemoveObstacleSound();
-        Instantiate(removeEffectPrefab, obstacleTilemap.GetCellCenterWorld(selectedCell), Quaternion.identity);
+        Instantiate(removeEffectPrefab, currentObstacleTilemap.GetCellCenterWorld(selectedCell), Quaternion.identity);
     }
 
     private void Update()
@@ -42,34 +47,66 @@ public class ObstacleRemover : MonoBehaviour
 
         Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int cellPosition = obstacleTilemap.WorldToCell(mousePosition);
+        bool hasObstacle = CheckObstaclesOnCellPosition(cellPosition);
 
-        if (cellPosition != lastHoveredCell && obstacleTilemap.GetTile(cellPosition) != null)
+        if (lastHoveredCell != cellPosition && hasObstacle && !isHovering)
         {
             AudioManager.Instance.PlayHoverSound();
+            isHovering = true;
+        }
+        else if (!hasObstacle)
+        {
+            isHovering = false;
         }
 
-        if (cellPosition != lastHoveredCell)
+        if (cellPosition != lastHoveredCell && currentObstacleTilemap != null)
         {
-            obstacleTilemap.SetColor(lastHoveredCell, Color.white);
+            currentObstacleTilemap.SetColor(lastHoveredCell, Color.white);
             lastHoveredCell = cellPosition;
         }
 
 
-        if (obstacleTilemap.GetTile(cellPosition) != null)
+        if (hasObstacle)
         {
-            obstacleTilemap.SetColor(cellPosition, Color.yellow);
+            if (currentObstacleTilemap!= null) currentObstacleTilemap.SetColor(cellPosition, Color.yellow);
         }
 
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
-            if (obstacleTilemap.GetTile(cellPosition) != null)
+            if (hasObstacle)
             {
                 contextMenuPanel.gameObject.SetActive(true);
-                Vector2 screenPosition = Input.mousePosition;
-                screenPosition = new Vector2(screenPosition.x, screenPosition.y - 240);
-                contextMenuPanel.anchoredPosition = screenPosition;
                 selectedCell = cellPosition;
             }
+        }
+    }
+
+    private bool CheckObstaclesOnCellPosition(Vector3Int cellPosition)
+    {
+        if (largeObstacleTilemap.GetTile(cellPosition) != null)
+        {
+            currentObstacleTilemap = largeObstacleTilemap;
+            removalCost = 500;
+            Debug.Log("The Removal Cost is 500 coins");
+            return true;
+        }
+        else if(middleObstacleTilemap.GetTile(cellPosition) != null)
+        {
+            currentObstacleTilemap = middleObstacleTilemap;
+            removalCost = 250;
+            Debug.Log("The Removal Cost is 250 coins");
+            return true;
+        }
+        else if (smallObstacleTilemap.GetTile(cellPosition) != null) 
+        {
+            currentObstacleTilemap = smallObstacleTilemap;
+            removalCost = 100;
+            Debug.Log("The Removal Cost is 100 coins");
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
